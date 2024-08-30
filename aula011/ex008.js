94c1c1252a4f0ed1de3f10e35a89fe9425b48044
DiogoSoares013 <carlosdiogo.fs@gmail.com>
2024-08-30 09:07:12 -0300

    praticando

---
+++ aula011/ex008.js
@@ -0,0 +1,5 @@
+var vel = 70
+if (vel > 60){
+    console.log(`Voce ultrapassou o limite de 60 km/h MULTADO passou a ${vel} km/h`)
+}
+console.log('Dirija sempre com segurança!')
---
+++ aula011/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'.toUpperCase()
+
+console.log(`Vivendo no ${pais}`)
+
+if (pais == 'BRASIL') {
+    console.log('Você é Brasileiro')
+}else{
+    console.log('Você é Estrangeiro')
+}
