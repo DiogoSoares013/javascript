7cc7331507e3e41d863026d8aa136e2cc10092ae
DiogoSoares013 <carlosdiogo.fs@gmail.com>
2024-08-31 10:36:04 -0300

    terminando projeto

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -10,12 +10,42 @@ function verificar() {
         fsex = document.getElementsByName('radsex') // pegando oque a pessoa marcou no radio
         idade = ano - Number(fano.value)
         genero = ''
+        img = document.createElement('img') // cria a tag imagem e depois coloca o id de foto
+        img.setAttribute('id', 'foto')
+
         if (fsex[0].checked){ // verificando oque a pessoa marcou e criando uma var em cima disso
             genero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                // criança
+                img.setAttribute('src', 'bebe.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovem-m.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'adulto-m.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'idoso-m.png')
+            }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 10){
+                // criança
+                img.setAttribute('src', 'bebe.png')
+            } else if (idade < 21) {
+                // jovem
+                img.setAttribute('src', 'jovem-f.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'adulto-f.png')
+            } else {
+                // idoso
+                img.setAttribute('src', 'idoso-f.png')
+            }
         }
-        res.style.textAling = 'center'
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img) // vai adicionar o elemento depois do texto acima 
     }
 }
