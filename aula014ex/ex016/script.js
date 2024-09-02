e5b481cdf1dfa979a04c046dde536b68c4403853
DiogoSoares013 <carlosdiogo.fs@gmail.com>
2024-09-02 09:45:29 -0300

    testando

---
+++ aula014ex/ex016/script.js
@@ -0,0 +1,9 @@
+function contar(){
+i = window.document.getElementById('inum1').value
+f = window.document.getElementById('inum2').value
+p = window.document.getElementById('inum3').value
+res = window.document.getElementById('res')
+
+res.innerHTML = `<p></p>`
+
+}
