2580872ed935d7b3c32ff681f2d6c4b3c078c1f3
DiogoSoares013 <carlosdiogo.fs@gmail.com>
2024-09-02 08:55:08 -0300

    praticando

--- aula013/teste.js
+++ aula013/teste.js
@@ -1,1 +1,17 @@
-console.log('ola mundo')
+// Teste logico no inicio
+
+
+/*var c = 1
+while(c <= 6) { // Enquanto c for menor que 6 o sistema vai rodar console.log repetidas vezes
+    console.log(`Repetiu e Somou ${c} vezes`)
+    c++ // aqui ele vai somar c + 1 a cada vez q o while rodar, ou seja, o sistema vai rodar 5 vezes ao todo ate cumprir com a condição acima
+}
+*/
+
+// Teste logico no final 
+
+c = 1
+do {
+    console.log(`Somou e repetiu ${c} vezes`)
+    c++
+} while (c <= 2)
