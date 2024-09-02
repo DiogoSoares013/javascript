647cacdd9486733c31c9454997efa912d3b6b8b8
DiogoSoares013 <carlosdiogo.fs@gmail.com>
2024-09-02 16:01:10 -0300

    terminando ex01

--- aula014ex/ex016/script.js
+++ aula014ex/ex016/script.js
@@ -1,9 +1,32 @@
-function contar(){
-i = window.document.getElementById('inum1').value
-f = window.document.getElementById('inum2').value
-p = window.document.getElementById('inum3').value
-res = window.document.getElementById('res')
+function contar () {
+    ini = window.document.getElementById('txti')
+    fim = window.document.getElementById('txtf')
+    passo = window.document.getElementById('txtp')
+    res = window.document.getElementById('res')
 
-res.innerHTML = `<p></p>`
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('ERRO faltam dados!')
+        res.innerHTML = 'impossivel contar!'
 
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        i = Number(ini.value)
+        f = Number(fim.value) // talvez tenha q colocar .value, (teve q colocar mesmo)
+        p = Number(passo.value)
+
+        if(p <= 0) {
+            window.alert('Passo Invalido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            for(c = i ; c <= f ; c+= p) { // enquanto c for menor igual ao fim (c e fim numero dado pelo usuario) ele vai somar com p (passos dado pelo usuario)
+            res.innerHTML += ` ${c} \u{1F449}` // detalhe no espaco dado por causa da concatenacao e += sendo usado para mostrar todos os dados gerados com a repeticao
+        }
+        } else {
+            for(c=i ; c >= f ; c -= p) { // enquanto o inicio for maior que o final o programa vai tirar (p) vezes ate que chegue no valor de (f)
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
