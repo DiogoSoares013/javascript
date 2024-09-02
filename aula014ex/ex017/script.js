2f428ef1a266d5dadd35800de03f9819cc3fa07a
DiogoSoares013 <carlosdiogo.fs@gmail.com>
2024-09-02 16:56:33 -0300

    adcionando comentarios

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -2,11 +2,11 @@ function calcular () {
     n = window.document.getElementById('inum')
     res = window.document.getElementById('res')
 
-    if (n.value.length == 0 || n.value == 0) {
+    if (n.value.length == 0 || n.value == 0) { // se ele nao colocar nada ou colocar 0 a tabuada nao vai ser feita
         alert('ERRO digite um valor')
     } else {
-        for(c=1;c<=10;c++) {
-            tab = n.value * c
+        for(c=1;c<=10;c++) { // fazendo um contador de 1 ate 10
+            tab = n.value * c // vai multiplicar a cada vez que o contador subir
             res.innerHTML += ` ${n.value} x ${c} = ${tab} <br>`
         }
     }
